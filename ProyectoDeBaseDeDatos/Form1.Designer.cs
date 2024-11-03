38f3dc5fb8a528b651ef9742369fe1049a8ad47c
Juan <s22120254@alumnos.itsur.edu.mx>
2024-11-03 10:17:29 -0600

    Borre todo

--- ProyectoDeBaseDeDatos/Form1.Designer.cs
+++ ProyectoDeBaseDeDatos/Form1.Designer.cs
@@ -28,35 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
-            button2 = new Button();
             SuspendLayout();
-            // 
-            // button1
-            // 
-            button1.Location = new Point(412, 103);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 0;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            button2.Location = new Point(417, 224);
-            button2.Name = "button2";
-            button2.Size = new Size(75, 23);
-            button2.TabIndex = 1;
-            button2.Text = "button2";
-            button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(button2);
-            Controls.Add(button1);
             Name = "Form1";
             Text = "Inicio";
             Load += Form1_Load;
@@ -64,8 +42,5 @@
         }
 
         #endregion
-
-        private Button button1;
-        private Button button2;
     }
 }
