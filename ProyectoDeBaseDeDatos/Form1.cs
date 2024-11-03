a14b4eb454bd8495f139d251aa9c25e2dd578c0f
Juan <s22120254@alumnos.itsur.edu.mx>
2024-11-02 20:04:26 -0600

    Comentario agregue una clase.

--- ProyectoDeBaseDeDatos/Form1.cs
+++ ProyectoDeBaseDeDatos/Form1.cs
@@ -6,5 +6,10 @@ namespace ProyectoDeBaseDeDatos
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
