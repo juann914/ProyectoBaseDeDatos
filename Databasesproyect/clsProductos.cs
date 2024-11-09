b7a10c6161c83af91c6a8b1e3c41e3baa5a5321d
Roy Niwre <nearjager@gmail.com>
2024-11-09 11:52:24 -0600

    cls producto

---
+++ Databasesproyect/clsProductos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Databasesproyect
+{
+    internal class clsProductos
+    {
+
+        public int idProducto { get; set; }
+        public string codigoBarra { get; set; }
+        public string nombre { get; set; }
+        public string precio { get; set; }
+        public string marca { get; set; }
+        
+    }
+}
