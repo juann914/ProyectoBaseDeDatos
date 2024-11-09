a36dda8dcee64932649f8ff63c2736a605a8079a
Roy Niwre <nearjager@gmail.com>
2024-11-09 09:47:43 -0600

    Forms

--- Databasesproyect/ProductosInsertar.cs
+++ Databasesproyect/ProductosInsertar.cs
@@ -16,5 +16,25 @@ namespace Databasesproyect
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
