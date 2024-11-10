ddc0ddf9ffd1af0c0bca0db3722d3117c68cdc63
Juan <s22120254@alumnos.itsur.edu.mx>
2024-11-10 15:12:48 -0600

    h

--- Databasesproyect/Menu.Designer.cs
+++ Databasesproyect/Menu.Designer.cs
@@ -35,39 +35,44 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(47, 153);
+            this.button1.Location = new System.Drawing.Point(35, 124);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(135, 55);
+            this.button1.Size = new System.Drawing.Size(101, 45);
             this.button1.TabIndex = 0;
             this.button1.Text = "Venta";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(269, 153);
+            this.button2.Location = new System.Drawing.Point(202, 124);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(135, 55);
+            this.button2.Size = new System.Drawing.Size(101, 45);
             this.button2.TabIndex = 1;
             this.button2.Text = "Productos";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(501, 153);
+            this.button3.Location = new System.Drawing.Point(376, 124);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(135, 55);
+            this.button3.Size = new System.Drawing.Size(101, 45);
             this.button3.TabIndex = 2;
             this.button3.Text = "Empleados";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Menu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(787, 287);
+            this.ClientSize = new System.Drawing.Size(590, 233);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Menu";
             this.Text = "Menu";
             this.ResumeLayout(false);
