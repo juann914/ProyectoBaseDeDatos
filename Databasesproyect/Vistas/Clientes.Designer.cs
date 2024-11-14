a5b2b37ff54110ba8ee9374866ba887ae2c1fa4c
Erwintm <s22120188@alumnos.itsur.edu.mx>
2024-11-13 21:02:31 -0600

    carpetas

--- Databasesproyect/Vistas/Clientes.Designer.cs
+++ Databasesproyect/Vistas/Clientes.Designer.cs
@@ -31,8 +31,9 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -59,24 +60,16 @@
             // groupBox1
             // 
             this.groupBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.groupBox1.Controls.Add(this.button4);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Controls.Add(this.button3);
             this.groupBox1.Controls.Add(this.button2);
             this.groupBox1.Location = new System.Drawing.Point(80, 47);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(426, 352);
+            this.groupBox1.Size = new System.Drawing.Size(426, 408);
             this.groupBox1.TabIndex = 3;
             this.groupBox1.TabStop = false;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(94, 221);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(198, 69);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Regresar";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
@@ -88,11 +81,29 @@
             this.label1.TabIndex = 4;
             this.label1.Text = "Elige una de las opciones";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(94, 306);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(198, 69);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Regresar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(94, 220);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(198, 69);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Solicitar datos";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
             // Clientes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(569, 437);
+            this.ClientSize = new System.Drawing.Size(569, 493);
             this.Controls.Add(this.groupBox1);
             this.Name = "Clientes";
             this.Text = "Clientes";
@@ -108,5 +119,6 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
     }
 }
