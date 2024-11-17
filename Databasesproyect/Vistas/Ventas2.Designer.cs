870c5685c640edcd2497c2879c9e3294be8481de
Erwintm <s22120188@alumnos.itsur.edu.mx>
2024-11-17 16:40:05 -0600

    d

---
+++ Databasesproyect/Vistas/Ventas2.Designer.cs
@@ -0,0 +1,394 @@
+﻿namespace Databasesproyect.Vistas
+{
+    partial class Ventas2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridProductos = new System.Windows.Forms.DataGridView();
+            this.IdProducto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CodigoBarra = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Precio = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Marca = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Descripcion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cantidad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textCodigo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.butBorrar = new System.Windows.Forms.Button();
+            this.butAgregar = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.laTotal = new System.Windows.Forms.Label();
+            this.laIVA = new System.Windows.Forms.Label();
+            this.laSubtotal = new System.Windows.Forms.Label();
+            this.laDesc = new System.Windows.Forms.Label();
+            this.teDes = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridProductos)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridProductos
+            // 
+            this.dataGridProductos.AllowUserToOrderColumns = true;
+            this.dataGridProductos.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dataGridProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridProductos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.IdProducto,
+            this.CodigoBarra,
+            this.Nombre,
+            this.Precio,
+            this.Marca,
+            this.Descripcion,
+            this.cantidad});
+            this.dataGridProductos.Location = new System.Drawing.Point(12, 102);
+            this.dataGridProductos.Name = "dataGridProductos";
+            this.dataGridProductos.RowHeadersWidth = 51;
+            this.dataGridProductos.RowTemplate.Height = 24;
+            this.dataGridProductos.Size = new System.Drawing.Size(1214, 241);
+            this.dataGridProductos.TabIndex = 1;
+            // 
+            // IdProducto
+            // 
+            this.IdProducto.HeaderText = "IdProducto";
+            this.IdProducto.MinimumWidth = 6;
+            this.IdProducto.Name = "IdProducto";
+            this.IdProducto.Width = 125;
+            // 
+            // CodigoBarra
+            // 
+            this.CodigoBarra.HeaderText = "CodigoBarra";
+            this.CodigoBarra.MinimumWidth = 6;
+            this.CodigoBarra.Name = "CodigoBarra";
+            this.CodigoBarra.Width = 125;
+            // 
+            // Nombre
+            // 
+            this.Nombre.HeaderText = "Nombre";
+            this.Nombre.MinimumWidth = 6;
+            this.Nombre.Name = "Nombre";
+            this.Nombre.Width = 125;
+            // 
+            // Precio
+            // 
+            this.Precio.HeaderText = "Precio";
+            this.Precio.MinimumWidth = 6;
+            this.Precio.Name = "Precio";
+            this.Precio.Width = 125;
+            // 
+            // Marca
+            // 
+            this.Marca.HeaderText = "Marca";
+            this.Marca.MinimumWidth = 6;
+            this.Marca.Name = "Marca";
+            this.Marca.Width = 125;
+            // 
+            // Descripcion
+            // 
+            this.Descripcion.HeaderText = "Descripcion";
+            this.Descripcion.MinimumWidth = 6;
+            this.Descripcion.Name = "Descripcion";
+            this.Descripcion.Width = 125;
+            // 
+            // cantidad
+            // 
+            this.cantidad.HeaderText = "Cantidad";
+            this.cantidad.MinimumWidth = 6;
+            this.cantidad.Name = "cantidad";
+            this.cantidad.Width = 125;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 60);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Cliente Nuevo";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(667, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ingresa id del cliente :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(824, 55);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textCodigo
+            // 
+            this.textCodigo.Location = new System.Drawing.Point(32, 404);
+            this.textCodigo.Margin = new System.Windows.Forms.Padding(4);
+            this.textCodigo.Name = "textCodigo";
+            this.textCodigo.Size = new System.Drawing.Size(132, 22);
+            this.textCodigo.TabIndex = 21;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 361);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 16);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "Codigo Producto";
+            // 
+            // butBorrar
+            // 
+            this.butBorrar.Location = new System.Drawing.Point(159, 443);
+            this.butBorrar.Margin = new System.Windows.Forms.Padding(4);
+            this.butBorrar.Name = "butBorrar";
+            this.butBorrar.Size = new System.Drawing.Size(100, 28);
+            this.butBorrar.TabIndex = 24;
+            this.butBorrar.Text = "Borrar";
+            this.butBorrar.UseVisualStyleBackColor = true;
+            // 
+            // butAgregar
+            // 
+            this.butAgregar.Location = new System.Drawing.Point(15, 443);
+            this.butAgregar.Margin = new System.Windows.Forms.Padding(4);
+            this.butAgregar.Name = "butAgregar";
+            this.butAgregar.Size = new System.Drawing.Size(100, 28);
+            this.butAgregar.TabIndex = 23;
+            this.butAgregar.Text = "Agregar";
+            this.butAgregar.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(507, 443);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 28);
+            this.button4.TabIndex = 25;
+            this.button4.Text = "Salir";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(380, 443);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 28);
+            this.button2.TabIndex = 26;
+            this.button2.Text = "Generar ticket";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.laTotal);
+            this.groupBox1.Controls.Add(this.laIVA);
+            this.groupBox1.Controls.Add(this.laSubtotal);
+            this.groupBox1.Controls.Add(this.laDesc);
+            this.groupBox1.Controls.Add(this.teDes);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Location = new System.Drawing.Point(824, 365);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(289, 155);
+            this.groupBox1.TabIndex = 27;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Totales";
+            // 
+            // laTotal
+            // 
+            this.laTotal.AutoSize = true;
+            this.laTotal.Location = new System.Drawing.Point(192, 114);
+            this.laTotal.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.laTotal.Name = "laTotal";
+            this.laTotal.Size = new System.Drawing.Size(34, 16);
+            this.laTotal.TabIndex = 15;
+            this.laTotal.Text = "$ 0.0";
+            this.laTotal.Click += new System.EventHandler(this.laTotal_Click);
+            // 
+            // laIVA
+            // 
+            this.laIVA.AutoSize = true;
+            this.laIVA.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.laIVA.Location = new System.Drawing.Point(192, 87);
+            this.laIVA.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.laIVA.Name = "laIVA";
+            this.laIVA.Size = new System.Drawing.Size(34, 16);
+            this.laIVA.TabIndex = 14;
+            this.laIVA.Text = "$ 0.0";
+            // 
+            // laSubtotal
+            // 
+            this.laSubtotal.AutoSize = true;
+            this.laSubtotal.Location = new System.Drawing.Point(192, 59);
+            this.laSubtotal.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.laSubtotal.Name = "laSubtotal";
+            this.laSubtotal.Size = new System.Drawing.Size(34, 16);
+            this.laSubtotal.TabIndex = 13;
+            this.laSubtotal.Text = "$ 0.0";
+            // 
+            // laDesc
+            // 
+            this.laDesc.AutoSize = true;
+            this.laDesc.Location = new System.Drawing.Point(192, 28);
+            this.laDesc.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.laDesc.Name = "laDesc";
+            this.laDesc.Size = new System.Drawing.Size(34, 16);
+            this.laDesc.TabIndex = 12;
+            this.laDesc.Text = "$ 0.0";
+            // 
+            // teDes
+            // 
+            this.teDes.Location = new System.Drawing.Point(64, 23);
+            this.teDes.Margin = new System.Windows.Forms.Padding(4);
+            this.teDes.Name = "teDes";
+            this.teDes.Size = new System.Drawing.Size(39, 22);
+            this.teDes.TabIndex = 11;
+            this.teDes.Text = "00";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(8, 114);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 16);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Total";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 89);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 16);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "IVA";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(8, 59);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "SubTotal";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(8, 28);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(54, 16);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "%Desc.";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(960, 55);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 28);
+            this.button3.TabIndex = 28;
+            this.button3.Text = "Aceptar";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Ventas2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1265, 533);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.butBorrar);
+            this.Controls.Add(this.butAgregar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textCodigo);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridProductos);
+            this.Name = "Ventas2";
+            this.Text = "Ventas2";
+            this.Load += new System.EventHandler(this.Ventas2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridProductos)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridProductos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn IdProducto;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CodigoBarra;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Precio;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Marca;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Descripcion;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cantidad;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textCodigo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button butBorrar;
+        private System.Windows.Forms.Button butAgregar;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label laTotal;
+        private System.Windows.Forms.Label laIVA;
+        private System.Windows.Forms.Label laSubtotal;
+        private System.Windows.Forms.Label laDesc;
+        private System.Windows.Forms.TextBox teDes;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button3;
+    }
+}
---
+++ Databasesproyect/Vistas/Ventas2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Databasesproyect.Vistas
+{
+    public partial class Ventas2 : Form
+    {
+        public Ventas2()
+        {
+            InitializeComponent();
+        }
+
+        private void Ventas2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void laTotal_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
