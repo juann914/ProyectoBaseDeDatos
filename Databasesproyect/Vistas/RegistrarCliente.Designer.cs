3faed769f71c924c2b303c2b70980b867c0521ad
Yahir <s22120159@alumnos.itsur.edu.mx>
2024-11-16 21:05:29 -0600

    c

---
+++ Databasesproyect/Vistas/RegistrarCliente.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Databasesproyect.Vistas
+{
+    partial class RegistrarCliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textNombre = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textApellidos = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textCorreo = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textRFC = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.butAgreC = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textNombre
+            // 
+            this.textNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textNombre.Location = new System.Drawing.Point(45, 41);
+            this.textNombre.Multiline = true;
+            this.textNombre.Name = "textNombre";
+            this.textNombre.Size = new System.Drawing.Size(186, 36);
+            this.textNombre.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(40, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(160, 25);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Nombre Cliente";
+            // 
+            // textApellidos
+            // 
+            this.textApellidos.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textApellidos.Location = new System.Drawing.Point(45, 112);
+            this.textApellidos.Multiline = true;
+            this.textApellidos.Name = "textApellidos";
+            this.textApellidos.Size = new System.Drawing.Size(186, 38);
+            this.textApellidos.TabIndex = 18;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(40, 80);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(100, 25);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Apellidos";
+            // 
+            // textCorreo
+            // 
+            this.textCorreo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textCorreo.Location = new System.Drawing.Point(45, 185);
+            this.textCorreo.Name = "textCorreo";
+            this.textCorreo.Size = new System.Drawing.Size(186, 31);
+            this.textCorreo.TabIndex = 20;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(40, 153);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(77, 25);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "Correo";
+            // 
+            // textRFC
+            // 
+            this.textRFC.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textRFC.Location = new System.Drawing.Point(45, 255);
+            this.textRFC.Name = "textRFC";
+            this.textRFC.Size = new System.Drawing.Size(186, 31);
+            this.textRFC.TabIndex = 22;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(42, 223);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(55, 25);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "RFC";
+            // 
+            // butAgreC
+            // 
+            this.butAgreC.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butAgreC.Location = new System.Drawing.Point(87, 292);
+            this.butAgreC.Name = "butAgreC";
+            this.butAgreC.Size = new System.Drawing.Size(86, 29);
+            this.butAgreC.TabIndex = 23;
+            this.butAgreC.Text = "Agregar";
+            this.butAgreC.UseVisualStyleBackColor = true;
+            // 
+            // RegistrarCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(316, 345);
+            this.Controls.Add(this.butAgreC);
+            this.Controls.Add(this.textRFC);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textCorreo);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textApellidos);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textNombre);
+            this.Controls.Add(this.label5);
+            this.Name = "RegistrarCliente";
+            this.Text = "RegistrarCliente";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textNombre;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textApellidos;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textCorreo;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textRFC;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button butAgreC;
+    }
+}
